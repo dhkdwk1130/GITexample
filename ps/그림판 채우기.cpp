7590f9fd35130bebba0fcd7c10faf09410e57ec3
dhkdwk1130 <dhkdwkaudgns@naver.com>
2018-12-14 22:26:17 +0900

    update paint ps

--- ps/그림판 채우기.cpp
+++ ps/그림판 채우기.cpp
@@ -1,9 +1,11 @@
-#include<stdio.h>
-#include<iostream>
-#include<string>
-using namespace std;
+/*
+c style code
+basic dfs
+2018-12-14
+*/
 
-string map[10];
+#include<stdio.h>
+char map[11][12];
 int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
 void solve(int x, int y)
@@ -25,13 +27,13 @@ void solve(int x, int y)
 int main()
 {
 	int x, y;
-	
+
 	for (int i = 0; i< 10; i++)
 	{
-		cin >> map[i];
+		scanf("%s", &map[i]);
 	}
 	scanf("%d %d", &x, &y);
-	if(map[y][x] != '*')
+	if (map[y][x] != '*')
 		solve(y, x); //x,y에서 색채우기 탐색 
 	for (int i = 0; i< 10; i++)
 	{
